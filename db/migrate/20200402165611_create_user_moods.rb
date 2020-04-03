738f6cd607f8a58d8af9c8bc95a06d4cea34b6b6
Catarina Pimentel <acatarinapimentel@gmail.com>
2020-04-04 00:48:28 +0100

    features

--- db/migrate/20200402165611_create_user_moods.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserMoods < ActiveRecord::Migration[6.0]
-  def change
-    create_table :user_moods do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20200402165824_create_mood_movies.rb
+++
@@ -1,8 +0,0 @@
-class CreateMoodMovies < ActiveRecord::Migration[6.0]
-  def change
-    create_table :mood_movies do |t|
-
-      t.timestamps
-    end
-  end
-end
